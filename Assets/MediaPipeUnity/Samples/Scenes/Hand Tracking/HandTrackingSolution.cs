5b0f1d470fe7ebc6d06928c395ad0e24375b51ce
maryamnawas <maryamnawas2002@gmail.com>
2024-02-23 13:38:20 +0530

    open and close handtracking game files
    dino game

--- Assets/MediaPipeUnity/Samples/Scenes/Hand Tracking/HandTrackingSolution.cs
+++ Assets/MediaPipeUnity/Samples/Scenes/Hand Tracking/HandTrackingSolution.cs
@@ -10,122 +10,103 @@ using UnityEngine;
 
 namespace Mediapipe.Unity.Sample.HandTracking
 {
-    public class HandTrackingSolution : ImageSourceSolution<HandTrackingGraph>
+  public class HandTrackingSolution : ImageSourceSolution<HandTrackingGraph>
+  {
+    [SerializeField] private DetectionListAnnotationController _palmDetectionsAnnotationController;
+    [SerializeField] private NormalizedRectListAnnotationController _handRectsFromPalmDetectionsAnnotationController;
+    [SerializeField] private MultiHandLandmarkListAnnotationController _handLandmarksAnnotationController;
+    [SerializeField] private NormalizedRectListAnnotationController _handRectsFromLandmarksAnnotationController;
+
+    public bool IsHandClosed = false;
+
+    public HandTrackingGraph.ModelComplexity modelComplexity
     {
-        [SerializeField] private DetectionListAnnotationController _palmDetectionsAnnotationController;
-        [SerializeField] private NormalizedRectListAnnotationController _handRectsFromPalmDetectionsAnnotationController;
-        [SerializeField] private MultiHandLandmarkListAnnotationController _handLandmarksAnnotationController;
-        [SerializeField] private NormalizedRectListAnnotationController _handRectsFromLandmarksAnnotationController;
+      get => graphRunner.modelComplexity;
+      set => graphRunner.modelComplexity = value;
+    }
 
-        private bool fingersClosed = false;
-        private bool jumpRequested = false;
+    public int maxNumHands
+    {
+      get => graphRunner.maxNumHands;
+      set => graphRunner.maxNumHands = value;
+    }
 
-        public HandTrackingGraph.ModelComplexity modelComplexity
-        {
-            get => graphRunner.modelComplexity;
-            set => graphRunner.modelComplexity = value;
-        }
+    public float minDetectionConfidence
+    {
+      get => graphRunner.minDetectionConfidence;
+      set => graphRunner.minDetectionConfidence = value;
+    }
 
-        public int maxNumHands
-        {
-            get => graphRunner.maxNumHands;
-            set => graphRunner.maxNumHands = value;
-        }
+    public float minTrackingConfidence
+    {
+      get => graphRunner.minTrackingConfidence;
+      set => graphRunner.minTrackingConfidence = value;
+    }
 
-        public float minDetectionConfidence
-        {
-            get => graphRunner.minDetectionConfidence;
-            set => graphRunner.minDetectionConfidence = value;
-        }
+protected override void OnStartRun()
+{
+    graphRunner.OnPalmDetectectionsOutput += OnPalmDetectionsOutput;
+    graphRunner.OnHandRectsFromPalmDetectionsOutput += OnHandRectsFromPalmDetectionsOutput;
+    graphRunner.OnHandLandmarksOutput += OnHandLandmarksOutput;
+    // TODO: render HandWorldLandmarks annotations
+    graphRunner.OnHandRectsFromLandmarksOutput += OnHandRectsFromLandmarksOutput;
+    graphRunner.OnHandednessOutput += OnHandednessOutput;
 
-        public float minTrackingConfidence
-        {
-            get => graphRunner.minTrackingConfidence;
-            set => graphRunner.minTrackingConfidence = value;
-        }
+    var imageSource = ImageSourceProvider.ImageSource;
+    SetupAnnotationController(_palmDetectionsAnnotationController, imageSource, true);
+    SetupAnnotationController(_handRectsFromPalmDetectionsAnnotationController, imageSource, true);
+    SetupAnnotationController(_handLandmarksAnnotationController, imageSource, true);
+    SetupAnnotationController(_handRectsFromLandmarksAnnotationController, imageSource, true);
+}
 
-        protected override void OnStartRun()
-        {
-            graphRunner.OnPalmDetectectionsOutput += OnPalmDetectionsOutput;
-            graphRunner.OnHandRectsFromPalmDetectionsOutput += OnHandRectsFromPalmDetectionsOutput;
-            graphRunner.OnHandLandmarksOutput += OnHandLandmarksOutput;
-            // TODO: render HandWorldLandmarks annotations
-            graphRunner.OnHandRectsFromLandmarksOutput += OnHandRectsFromLandmarksOutput;
-            graphRunner.OnHandednessOutput += OnHandednessOutput;
 
-            var imageSource = ImageSourceProvider.ImageSource;
-            SetupAnnotationController(_palmDetectionsAnnotationController, imageSource, true);
-            SetupAnnotationController(_handRectsFromPalmDetectionsAnnotationController, imageSource, true);
-            SetupAnnotationController(_handLandmarksAnnotationController, imageSource, true);
-            SetupAnnotationController(_handRectsFromLandmarksAnnotationController, imageSource, true);
-        }
-        protected override void AddTextureFrameToInputStream(TextureFrame textureFrame)
-        {
-            graphRunner.AddTextureFrameToInputStream(textureFrame);
-        }
+    protected override void AddTextureFrameToInputStream(TextureFrame textureFrame)
+    {
+      graphRunner.AddTextureFrameToInputStream(textureFrame);
+    }
 
-        protected override IEnumerator WaitForNextValue()
-        {
-            var task = graphRunner.WaitNext();
-            yield return new WaitUntil(() => task.IsCompleted);
+    protected override IEnumerator WaitForNextValue()
+    {
+      var task = graphRunner.WaitNext();
+      yield return new WaitUntil(() => task.IsCompleted);
 
-            var result = task.Result;
-            _palmDetectionsAnnotationController.DrawNow(result.palmDetections);
-            _handRectsFromPalmDetectionsAnnotationController.DrawNow(result.handRectsFromPalmDetections);
-            _handLandmarksAnnotationController.DrawNow(result.handLandmarks, result.handedness);
+      var result = task.Result;
+      _palmDetectionsAnnotationController.DrawNow(result.palmDetections);
+      _handRectsFromPalmDetectionsAnnotationController.DrawNow(result.handRectsFromPalmDetections);
+      _handLandmarksAnnotationController.DrawNow(result.handLandmarks, result.handedness);
+      // TODO: render HandWorldLandmarks annotations
+      _handRectsFromLandmarksAnnotationController.DrawNow(result.handRectsFromLandmarks);
+    }
 
-            // Check if fingers are closed and request jump
-            if (fingersClosed && !jumpRequested)
-            {
-              jumpRequested = true;
-              GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+    private void OnPalmDetectionsOutput(object stream, OutputStream<List<Detection>>.OutputEventArgs eventArgs)
+    {
+      var packet = eventArgs.packet;
+      var value = packet == null ? default : packet.Get(Detection.Parser);
+      _palmDetectionsAnnotationController.DrawLater(value);
+    }
 
-              if (playerObject != null) {
-                  Debug.Log("Player GameObject found.");
-                  dino dinoComponent = playerObject.GetComponent<dino>();
+    private void OnHandRectsFromPalmDetectionsOutput(object stream, OutputStream<List<NormalizedRect>>.OutputEventArgs eventArgs)
+    {
+      var packet = eventArgs.packet;
+      var value = packet == null ? default : packet.Get(NormalizedRect.Parser);
+      _handRectsFromPalmDetectionsAnnotationController.DrawLater(value);
+    }
 
-                  if (dinoComponent != null) {
-                      dinoComponent.RequestJump();
-                  } else {
-                      Debug.LogError("dino component not found on the Player GameObject.");
-                  }
-              } else {
-                  Debug.LogError("Player GameObject not found.");
-              }
-
-            }
-            else if (!fingersClosed && jumpRequested)
-            {
-                jumpRequested = false;
-            }
-
-            _handRectsFromLandmarksAnnotationController.DrawNow(result.handRectsFromLandmarks);
-        }
-
-        private void OnPalmDetectionsOutput(object stream, OutputStream<List<Detection>>.OutputEventArgs eventArgs)
-        {
-            var packet = eventArgs.packet;
-            var value = packet == null ? default : packet.Get(Detection.Parser);
-            _palmDetectionsAnnotationController.DrawLater(value);
-        }
-
-        private void OnHandRectsFromPalmDetectionsOutput(object stream, OutputStream<List<NormalizedRect>>.OutputEventArgs eventArgs)
-        {
-            var packet = eventArgs.packet;
-            var value = packet == null ? default : packet.Get(NormalizedRect.Parser);
-            _handRectsFromPalmDetectionsAnnotationController.DrawLater(value);
-        }
-
-        private void OnHandLandmarksOutput(object stream, OutputStream<List<NormalizedLandmarkList>>.OutputEventArgs eventArgs)
+    private void OnHandLandmarksOutput(object stream, OutputStream<List<NormalizedLandmarkList>>.OutputEventArgs eventArgs)
         {
             var packet = eventArgs.packet;
             var value = packet == null ? default : packet.Get(NormalizedLandmarkList.Parser);
 
             if (value != null && value.Count > 0 && value[0].Landmark.Count >= 21)
             {
-                // Check the state of all fingers together
-                bool allFingersOpen = true;
+                // Log the landmarks for debugging
+                for (int i = 0; i < value[0].Landmark.Count; i++)
+                {
+                    var landmark = value[0].Landmark[i];
+                    Debug.Log($"Landmark {i}: ({landmark.X}, {landmark.Y}, {landmark.Z})");
+                }
 
+                // Check the state of each finger
                 for (int i = 1; i <= 20; i += 4) // Start from THUMB_CMC (1) and check every 4th joint
                 {
                     Vector3 fingerTipPosition = new Vector3(value[0].Landmark[i + 3].X, value[0].Landmark[i + 3].Y, value[0].Landmark[i + 3].Z);
@@ -134,44 +115,36 @@ namespace Mediapipe.Unity.Sample.HandTracking
                     float fingerDistance = Vector3.Distance(fingerTipPosition, knucklePosition);
 
                     // Log the finger distance for debugging
-                    Debug.Log($"Finger {i / 4 + 1} Distance: {fingerDistance}");
+                    // Debug.Log($"Finger {i / 4 + 1} Distance: {fingerDistance}");
 
-                    // If any finger is closed, set allFingersOpen to false
-                    if (fingerDistance <= 0.05f)
+                    if (fingerDistance > 0.05f)
                     {
-                        allFingersOpen = false;
+                        Debug.Log($"Finger {i / 4 + 1} is open!");
+                        IsHandClosed = false;
                     }
-                }
-
-                if (allFingersOpen)
-                {
-                    Debug.Log("Hand is open");
-                }
-                else
-                {
-                    // Reset jump when fingers are closed
-                    fingersClosed = true;
-                    jumpRequested = false;
-
-                    Debug.Log("Hand is closed, dino jumping");
+                    else
+                    {
+                        Debug.Log($"Finger {i / 4 + 1} is closed!");
+                        IsHandClosed = true;
+                    }
                 }
             }
 
             _handLandmarksAnnotationController.DrawLater(value);
         }
 
-        private void OnHandRectsFromLandmarksOutput(object stream, OutputStream<List<NormalizedRect>>.OutputEventArgs eventArgs)
-        {
-            var packet = eventArgs.packet;
-            var value = packet == null ? default : packet.Get(NormalizedRect.Parser);
-            _handRectsFromLandmarksAnnotationController.DrawLater(value);
-        }
-
-        private void OnHandednessOutput(object stream, OutputStream<List<ClassificationList>>.OutputEventArgs eventArgs)
-        {
-            var packet = eventArgs.packet;
-            var value = packet == null ? default : packet.Get(ClassificationList.Parser);
-            _handLandmarksAnnotationController.DrawLater(value);
-        }
+    private void OnHandRectsFromLandmarksOutput(object stream, OutputStream<List<NormalizedRect>>.OutputEventArgs eventArgs)
+    {
+      var packet = eventArgs.packet;
+      var value = packet == null ? default : packet.Get(NormalizedRect.Parser);
+      _handRectsFromLandmarksAnnotationController.DrawLater(value);
     }
+
+    private void OnHandednessOutput(object stream, OutputStream<List<ClassificationList>>.OutputEventArgs eventArgs)
+    {
+      var packet = eventArgs.packet;
+      var value = packet == null ? default : packet.Get(ClassificationList.Parser);
+      _handLandmarksAnnotationController.DrawLater(value);
+    }
+  }
 }
--- Assets/MediaPipeUnity/Tutorial/Hello World/HelloWorld.cs
+++ Assets/MediaPipeUnity/Tutorial/Hello World/HelloWorld.cs
@@ -10,49 +10,5 @@ using UnityEngine;
 
 namespace Mediapipe.Unity.Tutorial
 {
-  public class HelloWorld : MonoBehaviour
-  {
-    private void Start()
-    {
-      var configText = @"
-input_stream: ""in""
-output_stream: ""out""
-node {
-  calculator: ""PassThroughCalculator""
-  input_stream: ""in""
-  output_stream: ""out1""
-}
-node {
-  calculator: ""PassThroughCalculator""
-  input_stream: ""out1""
-  output_stream: ""out""
-}
-";
-      var graph = new CalculatorGraph(configText);
-      var poller = graph.AddOutputStreamPoller<string>("out");
-      graph.StartRun();
-
-      for (var i = 0; i < 10; i++)
-      {
-        var input = Packet.CreateStringAt("Hello World!", i);
-        graph.AddPacketToInputStream("in", input);
-      }
-
-      graph.CloseInputStream("in");
-
-      var output = new Packet<string>();
-      while (poller.Next(output))
-      {
-        Debug.Log(output.Get());
-      }
-
-      graph.WaitUntilDone();
-
-      poller.Dispose();
-      graph.Dispose();
-      output.Dispose();
-
-      Debug.Log("Done");
-    }
-  }
+  public class HelloWorld : MonoBehaviour { }
 }
